218109c97f5131fc478bee6536c8a2ee2f41fe6f
spelc <spelciucmaxim@gmail.com>
2024-11-04 18:42:14 +0200

    Add project files.

---
+++ Class Figure/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+
+    abstract class Figure
+    {
+        public abstract double CalculateArea();
+
+        public abstract double CalculatePerimeter();
+    }
+
+    class Circle : Figure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+
+    class Rectangle : Figure
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (Width + Height);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            Console.WriteLine("Коло:");
+            Console.WriteLine($"Радіус: {circle.Radius}");
+            Console.WriteLine($"Площа: {circle.CalculateArea():F2}");
+            Console.WriteLine($"Периметр: {circle.CalculatePerimeter():F2}\n");
+
+            Rectangle rectangle = new Rectangle(4, 7);
+            Console.WriteLine("Прямокутник:");
+            Console.WriteLine($"Ширина: {rectangle.Width}");
+            Console.WriteLine($"Висота: {rectangle.Height}");
+            Console.WriteLine($"Площа: {rectangle.CalculateArea():F2}");
+            Console.WriteLine($"Периметр: {rectangle.CalculatePerimeter():F2}");
+        }
+    }
+
+
---
+++ Class Fruit/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+    class Fruit
+    {
+        public string Name { get; set; }
+
+        public Fruit(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Назва фрукта: {Name}");
+        }
+    }
+
+    class Apple : Fruit
+    {
+        public string Variety { get; set; } 
+        public string Color { get; set; }   
+
+        public Apple(string name, string variety, string color) : base(name)
+        {
+            Variety = variety;
+            Color = color;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Сорт: {Variety}");
+            Console.WriteLine($"Колір: {Color}");
+        }
+    }
+
+    class Pear : Fruit
+    {
+        public Pear(string name) : base(name)
+        {
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit fruit = new Fruit("Загальний фрукт");
+            fruit.DisplayInfo();
+            Console.WriteLine();
+
+            Apple apple = new Apple("Яблуко", "Гала", "Червоний");
+            apple.DisplayInfo();
+            Console.WriteLine();
+
+            Pear pear = new Pear("Груша");
+            pear.DisplayInfo();
+        }
+    }
+
